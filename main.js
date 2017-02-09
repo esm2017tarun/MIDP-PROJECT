3cfc06d19e06ffeef01ea868299ea75c6f10bbc4
Tarun Vishwakarma <esm2017tarun@gmail.com>
2017-02-09 11:36:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+// $(document).ready(function(){
+//     $('.deletesubscriber').on('click', deleteSubscriber);
+// });
+
+function deleteSubscriber(){
+   // event.preventDefault();
+
+    var confirmation = confirm('Are you sure?');
+
+    if(confirmation){
+        $.ajax({
+            type: 'DELETE',
+            url: '/subscriber/'+ $('.deletesubscriber').data('id')
+        }).done(function(response){
+            window.location.replace('/');
+
+        });
+    } else {
+        return false;
+    }
+
+}
+
